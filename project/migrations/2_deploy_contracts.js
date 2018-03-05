dde6b69f41bbbf44de44e099f8bdf25b0e092365
Mike Pumphrey <mike.pumphrey@consensys.net>
2018-03-05 13:52:35 -0800

    Don't need to specify resolver address values for ropsten

--- project/migrations/2_deploy_contracts.js
+++ project/migrations/2_deploy_contracts.js
@@ -5,12 +5,8 @@ module.exports = function(deployer, network, accounts) {
     if (network == "testing" || network == "development") {
       // Sometimes it's not this address! Check the ethereum-bridge output to be sure.
       resolver = "0x6f485c8bf6fc43ea212e93bbf8ce046c7f1cb475";
-    } else if (network === "ropsten") {
-      resolver = "0xc03A2615D5efaf5F49F60B7BB6583eaec212fdf1";
-    } else {
-      var err = "No migrations logic exists for network: " + network;
-      throw new Error(err);
     }
+    // resolver values for ropsten and other nets are contained in oraclizeAPI.sol
 
     deployer.deploy(Bet, resolver);
 };
