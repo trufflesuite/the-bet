2e1f9c01319b0c9dfddd7f567eb3f0a3842e0329
Mike Pumphrey <mike.pumphrey@consensys.net>
2018-05-09 13:00:40 -0700

    Adding another intermediate file

---
+++ intermediate_steps/truffle_4gaslimit.js
@@ -0,0 +1,18 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "3",
+      gas: 4000000
+    }
+  },
+  mocha: {
+    reporter: "eth-gas-reporter"
+  }
+};
